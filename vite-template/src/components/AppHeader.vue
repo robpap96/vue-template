f79790ccd72256bb5f5322072985853185b593f1
robpap96 <robertopapagni1996@outlook.it>
2022-11-28 17:46:46 +0100

    aggiornato template

---
+++ vite-template/src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<script>
+//import AppName  from './....AppName.vue'
+
+export default {
+    name: 'AppHeader',
+    components: {
+
+    },
+    defineProps :{
+        //msg: String
+    },
+    data(){
+        return {
+            //
+        }
+    },
+
+
+}
+</script>
+
+<template>
+    <!-- <AppName/> -->
+</template>
+
+<style scoped>
+
+</style>
---
+++ vite-template/src/components/AppMain.vue
@@ -0,0 +1,28 @@
+<script>
+//import AppName  from './....AppName.vue'
+
+export default {
+    name: 'AppMain',
+    components: {
+
+    },
+    defineProps :{
+        //msg: String
+    },
+    data(){
+        return {
+            //
+        }
+    },
+
+
+}
+</script>
+
+<template>
+    <!-- <AppName/> -->
+</template>
+
+<style scoped>
+
+</style>
